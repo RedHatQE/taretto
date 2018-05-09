d569df529b9c0417495219b04f7665a5f4dabc32
Pete Savage <psavage@redhat.com>
2018-05-09 12:12:03 +0100

    Added some initial files

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: Apache License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+LONG_DESC = """
+Targetted tooling for testing, currently offering tools for Web UI navigation and interaction.
+"""
+
+if __name__ == "__main__":
+    setup(
+        name="turret",
+        description="Targetted tooling for testing, currently offering tools for Web UI navigation and interaction.",
+        license="GPL",
+        url="https://github.com/RedHatQE/turret",
+        version="0.5",
+        author="RedHatQE",
+        author_email="psavage@redhat.com",
+        maintainer="Pete Savage",
+        maintainer_email="psavage@redhat.com",
+        keywords=['Testing', 'Widget', 'Navigation'],
+        long_description=LONG_DESC,
+        packages=['turret'],
+        package_dir={"": "src"},
+        classifiers=CLASSIFIERS,
+)
