c88983b432ea07f2bb99fcbdfe108324511c7c45
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2018-06-20 12:25:44 +0200

    add basic sentaku imports to tarett.context

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(
-        setup_requires=["setuptools_scm"],
-        use_scm_version=True,
-    )
+    setup(setup_requires=["setuptools_scm"], use_scm_version=True)
---
+++ src/taretto/context.py
@@ -0,0 +1,9 @@
+from sentaku import ImlementationContext as _SentakuImplementationContext
+from sentaku import ContextualMethod, ContextualProperty, Element, Collection
+
+__all__ = ["ContextualMethod", "ContextualProperty", "Element", "Collection"]
+
+
+# subclass to have a custom dectate registration point
+class ImplementationContext(_SentakuImplementationContext):
+    pass
