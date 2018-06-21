66d83ada38298d8c7f32e5fea26149a58b580a76
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2018-06-21 10:03:45 +0200

    leave package_dir in setup.py due to python2 bug

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(setup_requires=["setuptools_scm"], use_scm_version=True)
+    setup(
+        setup_requires=["setuptools_scm"], use_scm_version=True, package_dir={"": "src"}
+    )
