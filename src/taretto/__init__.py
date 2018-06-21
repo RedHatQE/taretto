924f7a9204ac3598947043b0a9ccc517dc232688
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2018-06-21 15:30:35 +0200

    add basic aliases for wt widgets and browser

--- src/taretto/__init__.py
+++ src/taretto/__init__.py
@@ -1,3 +1,13 @@
 from apipkg import initpkg as __initpkg
 
-__initpkg(__name__, {"navigate": "navmazing"})
+# fmt: off
+__initpkg(__name__, {
+    "navigate": "navmazing",
+    "ui": {
+        "core": "widgetastic.widget",  # todo: html widget library here, limit to base
+        "patternfly": "widgetastic.patternfly",  # dito
+
+        "View": "widgetastic.widget:View",
+        "Browser": "widgetastic.browser.Browser",
+    },
+})
